0fbb59b226332d26c0bbf50d6e37f280410b7ad0
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-10 23:02:59 -0700

    fixed theme bug and installed convex

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import Navbar from "./_components/navbar";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full" suppressHydrationWarning>
       <Navbar />
-      <main className="h-full pt-40">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="mootion-theme">
-          {children}
-        </ThemeProvider>
-      </main>
+      <main className="h-full pt-40">{children}</main>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="mootion-theme">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
