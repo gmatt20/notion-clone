e6c5b3f0a3640ad556af963b802cdbed43604d1d
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-12 18:07:57 -0700

    implemented empty notes section

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
-        src="/public/plant.svg"
+        src="/plant.svg"
         height="300"
         width="300"
         alt="Empty, make a new note!"
+        className="dark:hidden"
       />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Mootion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
