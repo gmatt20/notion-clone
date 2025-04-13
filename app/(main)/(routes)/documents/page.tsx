bd985d8db47d7dce4b117f25648c271e3e92dd6f
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-12 18:03:02 -0700

    imported svg

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>This is a protected page</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/public/plant.svg"
+        height="300"
+        width="300"
+        alt="Empty, make a new note!"
+      />
+    </div>
+  );
 };
 
 export default DocumentsPage;
